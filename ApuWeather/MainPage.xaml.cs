00b4d7a793118bc1aa6c0f7c09f7b24051ac2ab4
Igarashi com Sono <gui.igarashi@hotmail.com>
2024-04-18 09:02:12 -0300

    Ajustes em Layout e depois só finalizar

--- ApuWeather/MainPage.xaml.cs
+++ ApuWeather/MainPage.xaml.cs
@@ -1,12 +1,52 @@
-﻿namespace ApuWeather;
+﻿using System.Reflection.Emit;
+
+namespace ApuWeather;
 
 public partial class MainPage : ContentPage
 {
-	
+	Results Resultados;
+
+
+
 	public MainPage()
 	{
 		InitializeComponent();
+
+		 Resultados = new Results();
+		 LayoutTest();
+		 PreencherTela();
+
 	}
+	
+		void LayoutTest()
+			{
+				Resultados.temp=21;
+				Resultados.description="Nublado";
+				Resultados.city="Apucarana-PR";
+				Resultados.rain=88.2;
+				Resultados.humidity=88.2;
+				Resultados.sunrise="6:22";
+				Resultados.sunset="18:44";
+				Resultados.wind_speedy=3;
+				Resultados.wind_direction="373 N";
+				Resultados.moon_phase="Nov";
+
+			}
+
+		void PreencherTela()
+			{
+				labelTemp.Text= Resultados.temp.ToString();
+				labelSky.Text= Resultados.description;
+				labelCidade.Text= Resultados.city;
+				labelChuva.Text= Resultados.rain.ToString();
+				labelHumidade.Text= Resultados.humidity.ToString();
+				labelAmanhecer.Text= Resultados.sunrise;
+				labelAnoitecer.Text= Resultados.sunset;
+				labelForcaWind.Text= Resultados.wind_speedy.ToString();
+				labelDirecaoWind.Text= Resultados.wind_direction;
+				labelMoonFase.Text= Resultados.moon_phase;
+			}
+	
 
 
 }
--- ApuWeather/Results.cs
+++ ApuWeather/Results.cs
@@ -8,7 +8,7 @@ public string description {get; set;}
 
 public string city {get; set;}
 
-public int humidity {get; set;}
+public double humidity {get; set;}
 
 public double rain {get; set;}
 
@@ -16,9 +16,9 @@ public string sunrise {get; set;}
 
 public string sunset {get; set;}
 
-public string wind_speedy {get; set;}
+public int wind_speedy {get; set;}
 
-public int wind_direction {get; set;}
+public string wind_direction {get; set;}
 
 public string moon_phase {get; set;}
 
